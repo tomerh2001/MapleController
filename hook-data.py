27780c30758c9b98ff76a7548f360bbd9c49bff2
BuildTools <unconfigured@null.spigotmc.org>
2020-12-18 20:31:04 +0200

    commit message

---
+++ hook-data.py
@@ -0,0 +1,2 @@
+hiddenimports = ['numpy', 'os', 'subprocess', 'ctypes', 'win32gui', 'win32com', 'pyautogui', 'keyboard', 'json', 'time', 'PIL']
+# pyinstaller bot.py --onefile --additional-hooks-dir="hook-data.py"
